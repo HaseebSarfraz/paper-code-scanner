f9bc1c4df279f0231bea701a2b5d184c3a470978
sarfra32 <haseebk.sarfraz@mail.utoronto.ca>
2025-05-06 20:49:04 -0400

    tailwind works on practice file 2 in prac folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     "./index.html",              // root-level test page
     "./public/**/*.html",        // future pages in /public
-    "./src/**/*.{js,ts,jsx,tsx}" // future JS/TS/React files
+    "./src/**/*.{js,ts,jsx,tsx}", // future JS/TS/React files
+    "./html+ css practice/**/*.html"
   ],
   theme: { extend: {} },
   plugins: [],
